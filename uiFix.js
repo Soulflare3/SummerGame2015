43903a9e9963b77eece4de6efb5c6baf0da78627
Soulflare3 <robert@sf3soft.net>
2015-06-18 16:37:44 -0400

    Drop UI 20px to account for glare

--- uiFix.js
+++ uiFix.js
@@ -2,7 +2,7 @@
 // @name Soulflare3's MonsterGame UI Fix
 // @namespace https://github.com/Soulflare3/SummerGame2015
 // @description Quick JS/CSS fix for overlapping text in the top UI
-// @version 0.1
+// @version 0.2
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
@@ -12,14 +12,14 @@
 
 
 var timeui = document.getElementsByClassName('game_time')[0];
-timeui.style.top = "30px";
+timeui.style.top = "40px";
 timeui.style.left = "380px";
 
 var levelnumui = document.getElementsByClassName('level_container2')[0];
 levelnumui.style.left = "500px";
 
 var leveltimeui = document.getElementsByClassName('level_time')[0];
-leveltimeui.style.top = "30px";
+leveltimeui.style.top = "40px";
 leveltimeui.style.left = "800px";
 
 console.log("Soulflare3's MonsterGame UI Fix loaded");
